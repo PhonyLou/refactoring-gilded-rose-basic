0ca3a8dd4cc1f432be8ddfb35fd23ba581146402
Phony Lou <louqilin555@gmail.com>
2020-05-20 01:10:29 +0800

    wrap goods as function

--- src/main/java/cn/xpbootcamp/gilded_rose/GildedRose.java
+++ src/main/java/cn/xpbootcamp/gilded_rose/GildedRose.java
@@ -1,14 +1,18 @@
 package cn.xpbootcamp.gilded_rose;
 
 class GildedRose {
-    Item[] goods;
+    public Item[] getGoods() {
+        return goods;
+    }
+
+    private final Item[] goods;
 
     public GildedRose(Item[] goods) {
         this.goods = goods;
     }
 
     public void update_quality() {
-        for (Item goodsItem : goods) {
+        for (Item goodsItem : this.getGoods()) {
             if (goodsItem.isRegularGoods()) {
                 goodsItem.updateRegularGoodsWhenOneDayPassed();
             } else if (goodsItem.isAgedBrie()) {
